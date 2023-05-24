f020236f33884e49bdf2f28cd741629fb6c89b49
Adam Putra <111202113615@mhs.dinus.ac.id>
2023-05-24 11:32:04 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   profile.classList.remove('active');
+}
+
+let profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+   profile.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    profile.classList.remove('active');
+}
+
+function loader(){
+   document.querySelector('.loader').style.display = 'none';
+}
+
+function fadeOut(){
+   setInterval(loader, 2000);
+}
+
+window.onload = fadeOut;
